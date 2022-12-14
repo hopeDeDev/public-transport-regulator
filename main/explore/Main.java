2e66a9d5576ca62ea527faad67f52d1a41a39ae1
Hope <hnkabind021@gmail.com>
2022-12-14 11:55:03 +0200

    additional conditional statements

--- main/explore/Main.java
+++ main/explore/Main.java
@@ -5,15 +5,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner obj = new Scanner(System.in);
+        Scanner object = new Scanner(System.in);
         System.out.println("How many people?");
-        int numPpl = obj.nextInt();
+        int numPeople = object.nextInt();
         System.out.println("How many passengers?");
-        int numPasseng = obj.nextInt();
+        int numPassengers = object.nextInt();
 
-        if (numPpl <= 200 && numPasseng <= 15) {
-            System.out.println("We are ready to vay!");
 
+        if (numPeople <= 200 && numPassengers <= 15) {
+            System.out.println("We are ready to Go!");
+        }
+        else if (numPeople > 200 && numPassengers > 15) {
+            System.out.println("number of passengers must be lower than 16 and the number of people must be lower than 2001");
+        }
+        else if (numPeople > 200){            
+            System.out.println("number higher than 200 and risk of covid spreading");
+        }
+        else if (numPassengers >  15){
+        System.out.println("passengers are higher than 15 and a high risk of covid spreading");
+        }
+        else{
+            System.out.println("Input correcet values");
         }
 
     }
